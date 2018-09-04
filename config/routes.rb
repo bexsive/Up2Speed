9886f279b355df5b1dacebb1637b588aad211199
Dev <bexsive@gmail.com>
2018-09-04 14:57:57 -0700

    updated navbar, profile page links for QOL

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
     resources :meals
   end
 
+#  resources :meals do
+#    resources :foods
+#  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
